e6f48ee202d952eca64cca44e24723b48f316d68
Akshay Telang <akshayit22@gmail.com>
2024-06-10 23:54:52 +0530

    change 6

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.use( "/api/v1/profile", profileRoutes );
 app.get( "/", ( req, res ) =>
 {
   res.status( 200 ).json( {
-    message: "Welcome to the Blog App API \n change --> 5",
+    message: "Welcome to the Blog App API change --> 2",
   } );
   res.end();
 } );
